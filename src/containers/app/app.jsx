dc2fd92a9c516bb109e82518646033230b4223c0
Louis Ritchie <louiscritchie@gmail.com>
2018-03-16 14:58:46 -0700

    animate container transitions.

--- src/containers/app/app.jsx
+++ src/containers/app/app.jsx
@@ -6,15 +6,23 @@ import routes from 'src/routes.js'
 import './app.css'
 
 class App extends Component {
+  state = {
+    lightsOn: false // when lights off, opacity is 0.
+  }
+
+  _hitLightSwitch = () => this.setState({ lightsOn: !this.state.lightsOn })
+
   render() {
+    const { lightsOn } = this.state
+
     return (
       <div className="app">
         <Header />
 
-        <div className="container">
+        <div className={`container ${lightsOn ? 'lightsOn' : ''}`}>
           <Switch>
-            {routes.map(route => (
-              <Route path={route.path} component={route.component} />
+            {routes.map(({path, component: Component}) => (
+              <Route path={path} render={props => <Component {...props} hitLightSwitch={this._hitLightSwitch} />} />
             ))}
             <Redirect from='/*' to='/' />
           </Switch>
--- src/containers/projects/projects.jsx
+++ src/containers/projects/projects.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import './projects.css'
 
 class Projects extends Component {
+  componentDidMount = () => setTimeout(this.props.hitLightSwitch, 200)
+
+  componentWillUnmount = () => this.props.hitLightSwitch()
+
   render() {
     return (
       <div className="projects">
