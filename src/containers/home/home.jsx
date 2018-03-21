300775e01f7a477280a78dbe1cf37d0ff26826a1
Louis Ritchie <louiscritchie@gmail.com>
2018-03-20 20:48:01 -0700

    create a title section with my name and credentials.

--- src/containers/home/home.jsx
+++ src/containers/home/home.jsx
@@ -7,6 +7,12 @@ class Home extends Component {
   render() {
     return (
       <div className='home'>
+        <div className='splash'>
+          <h1>Louis Ritchie</h1>
+          <h2>Software Developer | Victoria, BC</h2>
+          <h3>B.Sc. Mathematics & Computer Science | Graduating 2019 | 18 months full-time development experience</h3>
+        </div>
+
         {items.map((item, i) => (
           <ValueProp {...item} index={i} key={item.slug} />
         ))}
